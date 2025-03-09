a9ffb398f31ed29a6831d042c7f1040fd3d93d95
Minseo Kim <kimminss0@outlook.kr>
2025-03-09 15:01:10 +0900

    feat(hw3): complete

--- hw3/hw3.ml
+++ hw3/hw3.ml
@@ -25,9 +25,9 @@ module Boolean : SCALAR with type t = bool = struct
 
   let zero = false
   let one = true
-  let ( ++ ) x y = raise NotImplemented
-  let ( ** ) x y = raise NotImplemented
-  let ( == ) x y = raise NotImplemented
+  let ( ++ ) x y = x || y
+  let ( ** ) x y = x && y
+  let ( == ) x y = x = y
 end
 
 (* Problem 1-2 *)
@@ -35,17 +35,26 @@ end
 
 module VectorFn (Scal : SCALAR) : VECTOR with type elem = Scal.t = struct
   type elem = Scal.t
-  type t = unit
+  type t = elem list
 
   exception VectorIllegal
 
-  let create _ = raise NotImplemented
-  let to_list _ = raise NotImplemented
-  let dim _ = raise NotImplemented
-  let nth _ = raise NotImplemented
-  let ( ++ ) _ _ = raise NotImplemented
-  let ( == ) _ _ = raise NotImplemented
-  let innerp _ _ = raise NotImplemented
+  let create xs = if xs = [] then raise VectorIllegal else xs
+  let to_list xs = xs
+  let dim xs = List.length xs
+
+  let nth xs n =
+    match List.nth_opt xs n with None -> raise VectorIllegal | Some x -> x
+
+  let ( ++ ) xs ys =
+    if List.compare_lengths xs ys <> 0 then raise VectorIllegal else xs @ ys
+
+  let ( == ) xs ys =
+    if List.compare_lengths xs ys <> 0 then raise VectorIllegal else xs = ys
+
+  let innerp xs ys =
+    if List.compare_lengths xs ys <> 0 then raise VectorIllegal
+    else List.fold_left Scal.( ++ ) Scal.zero (List.map2 Scal.( ** ) xs ys)
 end
 
 (* Problem 1-3 *)
@@ -53,19 +62,53 @@ end
 
 module MatrixFn (Scal : SCALAR) : MATRIX with type elem = Scal.t = struct
   type elem = Scal.t
-  type t = unit
+  type t = elem list list
 
   exception MatrixIllegal
 
-  let create _ = raise NotImplemented
-  let identity _ = raise NotImplemented
-  let dim _ = raise NotImplemented
-  let transpose _ = raise NotImplemented
-  let to_list _ = raise NotImplemented
-  let get _ _ _ = raise NotImplemented
-  let ( ++ ) _ _ = raise NotImplemented
-  let ( ** ) _ _ = raise NotImplemented
-  let ( == ) _ _ = raise NotImplemented
+  let create mat =
+    let dim = List.length mat in
+    let pred = fun l -> List.compare_length_with l dim = 0 in
+    if dim > 0 && List.for_all pred mat then mat else raise MatrixIllegal
+
+  let identity dim =
+    if dim <= 0 then raise MatrixIllegal
+    else
+      List.init dim (fun n ->
+          List.init dim (fun k -> if k = n then Scal.one else Scal.zero))
+
+  let dim mat = List.length mat
+
+  let transpose mat =
+    List.init (dim mat) (fun j -> List.map (fun row -> List.nth row j) mat)
+
+  let to_list mat = mat
+
+  let get m r c =
+    let n = dim m in
+    if r < 0 || r >= n || c < 0 || c >= n then raise MatrixIllegal
+    else List.nth (List.nth m r) c
+
+  let ( ++ ) m m' =
+    if dim m <> dim m' then raise MatrixIllegal
+    else List.map2 (List.map2 Scal.( ++ )) m m'
+
+  module ScalVector = VectorFn (Scal)
+
+  let ( ** ) m m' =
+    if dim m <> dim m' then raise MatrixIllegal
+    else
+      let m't = transpose m' in
+      List.map
+        (fun row ->
+          List.map
+            (fun col -> ScalVector.(innerp (create row) (create col)))
+            m't)
+        m
+
+  let ( == ) m m' =
+    try List.for_all2 (fun r r' -> ScalVector.(create r == create r')) m m'
+    with ScalVector.VectorIllegal -> raise MatrixIllegal
 end
 
 (* Problem 2-1 *)
@@ -74,7 +117,13 @@ end
 module ClosureFn (Mat : MATRIX) : sig
   val closure : Mat.t -> Mat.t
 end = struct
-  let closure _ = raise NotImplemented
+  let closure m =
+    let ident = Mat.(identity (dim m)) in
+    let rec go c =
+      let c' = Mat.(ident ++ (c ** m)) in
+      if Mat.(c == c') then c else go c'
+    in
+    go ident
 end
 
 (* Problem 2-2 *)
@@ -83,7 +132,9 @@ end
 module BoolMat = MatrixFn (Boolean)
 module BoolMatClosure = ClosureFn (BoolMat)
 
-let reach _ = raise NotImplemented
+let reach adj =
+  try BoolMat.create adj |> BoolMatClosure.closure |> BoolMat.to_list
+  with BoolMat.MatrixIllegal -> raise IllegalFormat
 
 let al =
   [
@@ -110,16 +161,32 @@ module Distance : SCALAR with type t = int = struct
 
   exception ScalarIllegal
 
-  let zero = 999999 (* Dummy value : Rewrite it! *)
-  let one = 999999 (* Dummy value : Rewrite it! *)
-  let ( ++ ) _ _ = raise NotImplemented
-  let ( ** ) _ _ = raise NotImplemented
-  let ( == ) _ _ = raise NotImplemented
+  let zero = -1
+  let one = 0
+  let check_valid x y = if x < -1 || y < -1 then raise ScalarIllegal
+
+  let ( ++ ) x y =
+    check_valid x y;
+    match (x, y) with -1, _ -> y | _, -1 -> x | _ -> min x y
+
+  let ( ** ) x y =
+    check_valid x y;
+    if x = -1 || y = -1 then -1 else x + y
+
+  let ( == ) x y =
+    check_valid x y;
+    x = y
 end
 
 (* .. Write some code here .. *)
 
-let distance _ = raise NotImplemented
+module DistanceMat = MatrixFn (Distance)
+module DistanceMatClosure = ClosureFn (DistanceMat)
+
+let distance adj =
+  try
+    DistanceMat.create adj |> DistanceMatClosure.closure |> DistanceMat.to_list
+  with DistanceMat.MatrixIllegal -> raise IllegalFormat
 
 let dl =
   [
@@ -146,16 +213,34 @@ module Weight : SCALAR with type t = int = struct
 
   exception ScalarIllegal
 
-  let zero = 999999 (* Dummy value : Rewrite it! *)
-  let one = 999999 (* Dummy value : Rewrite it! *)
-  let ( ++ ) _ _ = raise NotImplemented
-  let ( ** ) _ _ = raise NotImplemented
-  let ( == ) _ _ = raise NotImplemented
+  let zero = 0
+  let one = -1
+  let check_valid x y = if x < -1 || y < -1 then raise ScalarIllegal
+
+  let ( ++ ) x y =
+    check_valid x y;
+    match (x, y) with -1, _ | _, -1 -> -1 | _ -> max x y
+
+  let ( ** ) x y =
+    check_valid x y;
+    match (x, y) with
+    | -1, _ -> y
+    | _, -1 -> x
+    | _ -> min x y
+
+  let ( == ) x y =
+    check_valid x y;
+    x = y
 end
 
 (* .. Write some code here .. *)
 
-let weight _ = raise NotImplemented
+module WeightMat = MatrixFn (Weight)
+module WeightMatClosure = ClosureFn (WeightMat)
+
+let weight adj =
+  try WeightMat.create adj |> WeightMatClosure.closure |> WeightMat.to_list
+  with WeightMat.MatrixIllegal -> raise IllegalFormat
 
 let ml =
   [
