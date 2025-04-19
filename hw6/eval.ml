533a39ac17582f7b08240e7c9f11baab80e454b3
Minseo Kim <kimminss0@outlook.kr>
2025-04-19 16:28:13 +0900

    feat(hw6): implement part 2

--- hw6/eval.ml
+++ hw6/eval.ml
@@ -40,8 +40,7 @@ let texp2exp te =
         |> findFreeVar te1 |> List.tl |> findFreeVar te
     | Tifthenelse (te, te1, te2) ->
         cxt |> findFreeVar te2 |> findFreeVar te1 |> findFreeVar te
-    | Tnum _ -> cxt
-    | Teunit | Ttrue | Tfalse | Tplus | Tminus | Teq -> cxt
+    | Teunit | Ttrue | Tfalse | Tnum _ | Tplus | Tminus | Teq -> cxt
   in
   let rec texp2exp' cxt = function
     | Tvar v -> Ind (cxt |> List.mapi (fun idx v -> (v, idx)) |> List.assoc v)
@@ -72,7 +71,76 @@ let texp2exp te =
 
 (* Problem 2. 
  * step1 : Tml.exp -> Tml.exp *)
-let rec step1 _ = raise Stuck
+let shift =
+  let rec shift' i v = function
+    | Ind v' when v' >= i -> Ind (v' + v)
+    | Ind v' -> Ind v'
+    | Lam e -> Lam (shift' (i + 1) v e)
+    | App (e1, e2) -> App (shift' i v e1, shift' i v e2)
+    | Pair (e1, e2) -> Pair (shift' i v e1, shift' i v e2)
+    | Fst e -> Fst (shift' i v e)
+    | Snd e -> Snd (shift' i v e)
+    | Inl e -> Inl (shift' i v e)
+    | Inr e -> Inr (shift' i v e)
+    | Ifthenelse (e, e1, e2) ->
+        Ifthenelse (shift' i v e, shift' i v e1, shift' i v e2)
+    | Case (e, e1, e2) ->
+        Case (shift' i v e, shift' (i + 1) v e1, shift' (i + 1) v e2)
+    | Fix e -> Fix (shift' (i + 1) v e)
+    | (Eunit | True | False | Num _ | Plus | Minus | Eq) as e -> e
+  in
+  shift' 0
+
+let substitute =
+  let rec substitute' v m n =
+    match m with
+    | Ind v' when v' < v -> Ind v'
+    | Ind v' when v' = v -> shift v' n
+    | Ind v' (* when v' > v *) -> Ind (v' - 1)
+    | Lam e -> Lam (substitute' (v + 1) e n)
+    | App (e1, e2) -> App (substitute' v e1 n, substitute' v e2 n)
+    | Pair (e1, e2) -> Pair (substitute' v e1 n, substitute' v e2 n)
+    | Fst e -> Fst (substitute' v e n)
+    | Snd e -> Snd (substitute' v e n)
+    | Inl e -> Inl (substitute' v e n)
+    | Inr e -> Inr (substitute' v e n)
+    | Ifthenelse (e, e1, e2) ->
+        Ifthenelse (substitute' v e n, substitute' v e1 n, substitute' v e2 n)
+    | Case (e, e1, e2) ->
+        Case
+          (substitute' v e n, substitute' (v + 1) e1 n, substitute' (v + 1) e2 n)
+    | Fix e -> Fix (substitute' (v + 1) e n)
+    | (Eunit | True | False | Num _ | Plus | Minus | Eq) as e -> e
+  in
+  substitute' 0
+
+let rec step1 = function
+  | App ((Lam m as f), e) -> (
+      try App (f, step1 e) with Stuck -> substitute m e)
+  | App (Plus, Pair (Num n1, Num n2)) -> Num (n1 + n2)
+  | App (Plus, e) -> App (Plus, step1 e)
+  | App (Minus, Pair (Num n1, Num n2)) -> Num (n1 - n2)
+  | App (Minus, e) -> App (Minus, step1 e)
+  | App (Eq, Pair (Num n1, Num n2)) -> if n1 = n2 then True else False
+  | App (Eq, e) -> App (Minus, step1 e)
+  | App (e1, e2) -> App (step1 e1, e2)
+  | Pair (e1, e2) -> (
+      try Pair (step1 e1, e2) with Stuck -> Pair (e1, step1 e2))
+  | Fst (Pair (v1, v2)) -> v1
+  | Fst e -> Fst (step1 e)
+  | Snd (Pair (v1, v2)) -> v2
+  | Snd e -> Snd (step1 e)
+  | Inl e -> Inl (step1 e)
+  | Inr e -> Inr (step1 e)
+  | Ifthenelse (e, e1, e2) -> (
+      match e with True -> e1 | False -> e2 | _ -> Ifthenelse (step1 e, e1, e2))
+  | Case (e, e1, e2) -> (
+      match e with
+      | Inl v -> substitute e1 v
+      | Inr v -> substitute e2 v
+      | _ -> Case (step1 e, e1, e2))
+  | Fix e as f -> substitute e f
+  | _ -> raise Stuck
 
 (* Problem 3. 
  * step2 : state -> state *)
