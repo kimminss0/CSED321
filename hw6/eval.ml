1fa76b968619ff271cf3a24815816d08e358a9a2
Minseo Kim <kimminss0@outlook.kr>
2025-04-19 14:23:22 +0900

    style(hw6): run formatter

--- hw6/eval.ml
+++ hw6/eval.ml
@@ -1,24 +1,20 @@
 open Tml
-exception NotImplemented 
+
+exception NotImplemented
 exception Stuck
 exception NotConvertible
 
-type stoval = 
-    Computed of value 
-  | Delayed of exp * env
+type stoval = Computed of value | Delayed of exp * env
+and stack = Hole_SK | Frame_SK of stack * frame
 
- and stack =
-   Hole_SK
-   | Frame_SK of stack * frame
+and state =
+  | Anal_ST of stoval Heap.heap * stack * exp * env
+  | Return_ST of stoval Heap.heap * stack * value
 
- and state =
-   Anal_ST of (stoval Heap.heap) * stack * exp * env
-   | Return_ST of (stoval Heap.heap) * stack * value 
-
- (* Define your own datatypes *)
- and env = NOT_IMPLEMENT_ENV
- and value = NOT_IMPLEMENT_VALUE
- and frame = NOT_IMPLEMENT_FRAME
+(* Define your own datatypes *)
+and env = NOT_IMPLEMENT_ENV
+and value = NOT_IMPLEMENT_VALUE
+and frame = NOT_IMPLEMENT_FRAME
 
 (* Define your own empty environment *)
 let emptyEnv = NOT_IMPLEMENT_ENV
@@ -33,60 +29,64 @@ let value2exp _ = raise NotImplemented
 let texp2exp _ = raise NotImplemented
 
 (* Problem 2. 
- * step1 : Tml.exp -> Tml.exp *)   
+ * step1 : Tml.exp -> Tml.exp *)
 let rec step1 _ = raise NotImplemented
 
 (* Problem 3. 
  * step2 : state -> state *)
 let step2 _ = raise NotImplemented
-                    
+
 (* exp2string : Tml.exp -> string *)
-let rec exp2string exp = 
-  match exp with 
-    Ind x -> string_of_int x
-  | Lam e -> "(lam. " ^ (exp2string e) ^ ")"
-  | App (e1, e2) -> "(" ^ (exp2string e1) ^ " " ^ (exp2string e2) ^ ")"
-  | Pair (e1, e2) -> "(" ^ (exp2string e1) ^ "," ^ (exp2string e2) ^ ")"
-  | Fst e -> "(fst " ^ (exp2string e) ^ ")"
-  | Snd e -> "(snd " ^ (exp2string e) ^ ")"
+let rec exp2string exp =
+  match exp with
+  | Ind x -> string_of_int x
+  | Lam e -> "(lam. " ^ exp2string e ^ ")"
+  | App (e1, e2) -> "(" ^ exp2string e1 ^ " " ^ exp2string e2 ^ ")"
+  | Pair (e1, e2) -> "(" ^ exp2string e1 ^ "," ^ exp2string e2 ^ ")"
+  | Fst e -> "(fst " ^ exp2string e ^ ")"
+  | Snd e -> "(snd " ^ exp2string e ^ ")"
   | Eunit -> "()"
-  | Inl e -> "(inl " ^ (exp2string e) ^ ")"
-  | Inr e -> "(inr " ^ (exp2string e) ^ ")"
-  | Case (e, e1, e2) -> "(case " ^ (exp2string e) ^" of " ^ (exp2string e1) ^
-                          " | " ^ (exp2string e2) ^ ")"
-  | Fix e -> "(fix. "  ^ (exp2string e) ^ ")"
-  | Ifthenelse (e, e1, e2) -> 
-     "(if " ^ (exp2string e) ^ " then " ^ (exp2string e1) ^ 
-       " else " ^ (exp2string e2) ^ ")"
-  | True -> "true"  | False -> "false"
-  | Num n -> "<" ^ (string_of_int n) ^ ">"
-  | Plus -> "+"  | Minus -> "-" | Eq -> "="
+  | Inl e -> "(inl " ^ exp2string e ^ ")"
+  | Inr e -> "(inr " ^ exp2string e ^ ")"
+  | Case (e, e1, e2) ->
+      "(case " ^ exp2string e ^ " of " ^ exp2string e1 ^ " | " ^ exp2string e2
+      ^ ")"
+  | Fix e -> "(fix. " ^ exp2string e ^ ")"
+  | Ifthenelse (e, e1, e2) ->
+      "(if " ^ exp2string e ^ " then " ^ exp2string e1 ^ " else "
+      ^ exp2string e2 ^ ")"
+  | True -> "true"
+  | False -> "false"
+  | Num n -> "<" ^ string_of_int n ^ ">"
+  | Plus -> "+"
+  | Minus -> "-"
+  | Eq -> "="
 
 (* state2string : state -> string 
  * you may modify this function for debugging your code *)
-let state2string st = match st with
-    Anal_ST(_,_,exp,_) -> "Analysis : ???"
-  | Return_ST(_,_,_) -> "Return : ??? "
+let state2string st =
+  match st with
+  | Anal_ST (_, _, exp, _) -> "Analysis : ???"
+  | Return_ST (_, _, _) -> "Return : ??? "
 
-(* ------------------------------------------------------------- *)     
+(* ------------------------------------------------------------- *)
 let stepOpt1 e = try Some (step1 e) with Stuck -> None
 let stepOpt2 st = try Some (step2 st) with Stuck -> None
-
 let rec multiStep1 e = try multiStep1 (step1 e) with Stuck -> e
 let rec multiStep2 st = try multiStep2 (step2 st) with Stuck -> st
 
 let stepStream1 e =
-  let rec steps e = 
-    match (stepOpt1 e) with
-      None -> Stream.from (fun _ -> None)
+  let rec steps e =
+    match stepOpt1 e with
+    | None -> Stream.from (fun _ -> None)
     | Some e' -> Stream.icons e' (steps e')
-  in 
+  in
   Stream.icons e (steps e)
 
 let stepStream2 st =
-  let rec steps st = 
-    match (stepOpt2 st) with
-      None -> Stream.from (fun _ -> None)
+  let rec steps st =
+    match stepOpt2 st with
+    | None -> Stream.from (fun _ -> None)
     | Some st' -> Stream.icons st' (steps st')
-  in 
+  in
   Stream.icons st (steps st)
